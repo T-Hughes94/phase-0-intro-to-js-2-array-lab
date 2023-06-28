3b7a81dce8a6388d2df7c10600a3b710f816c114
Terence Hughes <Terencehughes450@gmail.com>
2023-06-28 15:47:20 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+ cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.push(name);
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift(name);
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.shift();
+    return copyOfCats
+}
